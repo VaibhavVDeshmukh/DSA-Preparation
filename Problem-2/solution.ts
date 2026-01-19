014451a7c6768d8ed3a01db50667af5545230c53
Vaibhav Deshmukh <vaibhav.deshmukh@ceinsys.com>
2026-01-19 21:13:17 +0530

    corrected solution of problem2

--- Problem-2/solution.ts
+++ Problem-2/solution.ts
@@ -2,7 +2,7 @@ const input:number[][] = [[1,1,1,0,0,0],[0,1,0,0,0,0],[1,1,1,0,0,0],[0,0,2,4,4,0
 //output should be 19
 
 const findMaxHourGlassSum = (arr: number[][]): number => {
-    let max: number = 0;
+    let max: number = -Infinity;
     let curr: number = 0;
     for (let i = 0; i < arr.length; i++) {
         if (arr[i+2]) {
